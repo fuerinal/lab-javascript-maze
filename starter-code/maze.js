3107b3c2da3a72783b41effb828f0a6f58bbc48b
Alejandro de la Fuente <alex695afr@gmail.com>
2017-06-23 10:14:53 +0200

    Commit

--- starter-code/maze.js
+++ starter-code/maze.js
@@ -8,28 +8,60 @@ var Maze = function(map, exit) {
   this._exit = {
     row: exit.row,
     col: exit.col
-  }
-}
+  };
+};
 
 Maze.prototype.turnLeft      = function(){
-}
+  this._miner.dir--;
+  if (this._miner.dir<0) {
+
+this._miner.dir=3;
+  }
+};
 
 Maze.prototype.turnRight     = function(){
-}
+  this._miner.dir++;
+
+  if (this._miner.dir==4) {
+
+this._miner.dir=0;
+  }
+
+};
 
 Maze.prototype.isPathForward = function(){
-}
+  if (this._miner.row-this._miner.col==-2) {
+    return true;
+
+  }
+  else {
+    return false;
+  }
+  if (this._miner.col-this._miner.row==2) {
+      return true;
+
+  }
+  else {
+    return false;
+  }
+
+
+};
 
 Maze.prototype.isPathLeft    = function(){
-}
+  if (this._miner.dir == 3) {
+  return true;
+  }
+  return false;
+};
 
 Maze.prototype.isPathRight   = function(){
-}
+};
 
 Maze.prototype.moveForward   = function(){
-}
+};
 
 Maze.prototype.notDone       = function(){
-}
+};
 
 module.exports = Maze;
